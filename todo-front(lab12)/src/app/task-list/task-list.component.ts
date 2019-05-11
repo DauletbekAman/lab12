7120a957d64349b8ef6b9c58518cb9e9a4ab0391
DauletbekAman <amankeldinov.d98@gmail.com>
2019-05-11 15:29:45 +0600

    fl

---
+++ todo-front(lab12)/src/app/task-list/task-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {ProviderService} from '../data/services/provider.service';
+import {ITaskList} from '../data/models';
+import {Location} from '@angular/common';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  public taskLists: ITaskList[] = [];
+  public taskListName: string = '';
+
+  constructor(
+    private provider: ProviderService,
+    private location: Location
+    ) {
+
+  }
+
+  ngOnInit() {
+      this.provider.getTaskLists().then(res => {
+      this.taskLists = res;
+    });
+  }
+
+  navigateBack() {
+    this.location.back();
+  }
+
+  createTaskList() {
+    if (this.taskListName.length > 0) {
+      this.provider.createTaskList(this.taskListName).then(res => {
+        this.taskLists.push(res);
+        this.taskListName = '';
+      });
+    }
+  }
+
+
+
+}
